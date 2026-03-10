1344e45d1c4aa35741002e8363409177ab06f92c
peppe2412 <peppeterrazzano79@gmail.com>
2026-03-10 01:31:50 +0100

    Fixed icons for deploying

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // export default function Heading() {
 //   return (
 //     <>
@@ -22,3 +23,29 @@
 //     </>
 //   );
 // }
+=======
+export default function Heading() {
+  return (
+    <>
+      <section className="heading-section">
+        <h1>How's the sky looking today?</h1>
+        <div className="input-container">
+          <object
+            className="icon-search"
+            data="./assets/images/icon-search.svg"
+            type="image/svg+xml"
+          >
+            Icon search
+          </object>
+          <input
+            name="searchMeteo"
+            type="text"
+            placeholder="Search for a place..."
+          />
+          <button className="button-search">Search</button>
+        </div>
+      </section>
+    </>
+  );
+}
+>>>>>>> 0055cab (Fixed icons for deploying)
